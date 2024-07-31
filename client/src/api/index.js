ce6baff5cdfae2d5f870467e82eef3c0ec56a29c
sarveshk0 <kumarsarvesh105@gmail.com>
2024-07-31 17:28:05 +0530

    changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,7 +9,7 @@
 // api.js
 import axios from 'axios';
 
-const url='http://localhost:5000/posts' // Replace with your API URL
+const url='https://memories-0jb9.onrender.com/' // Replace with your API URL
 export const getPost=(id)=>axios.get(`${url}/${id}`)
 export const fetchAllPosts = (page) =>axios.get(`${url}?page=${page}`);
 export const fetchPostBySearch=(searchQuery)=>axios.get(`${url}/search?searchQuery=${searchQuery || 'none'} `);
