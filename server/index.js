242317caff86e3ad7f09fa0437097df2e7dbdfae
SARVESH KUMAR <133014387+sarveshk0@users.noreply.github.com>
2026-04-26 15:03:10 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.use(bodyParser.json({ limit: '50mb' })); // Increase the payload size limit
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // Increase the payload size limit
 
 
+app.get('/', (req, res) => {
+    res.send("backend is connected and live");
+});
+
 app.get('/user', (req, res) => {
     res.status(200).json({ message: "valid hai ye" });
 });
@@ -27,9 +31,8 @@ dotenv.config()
 const CONNECTION_URL = process.env.MONGODB_URI;
 const PORT=process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:'true',useUnifiedTopology: 'true'})
+mongoose.connect(CONNECTION_URL)
 .then(()=>app.listen(PORT,()=>console.log(`App is listen at Port:${PORT}`)))
 .catch((error)=> console.log(error.message));
-
 
 // mongoose.set('useFindAndModify',false);
